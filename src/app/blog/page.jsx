c894583bb0b9563956910c0bb43bbe0570dab6f1
Ashahab007 <ashahab007@gmail.com>
2025-11-15 23:34:55 +0600

    first commit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import BlogCard from "../../components/BlogCard";
 
-export default function BlogPage() {
+export default async function BlogPage() {
   // 10.1 make some fake blog data
-  const blogsCollection = [
+  /* const blogsCollection = [
     {
       id: 1,
       title: "Blog 1",
@@ -20,22 +21,29 @@ export default function BlogPage() {
       title: "Blog 3",
       description: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
     },
-  ];
+  ]; */
 
+  // console.log(blogsCollection);
+
+  // 11.0 now we are going to work with fetching data which is only perform in server component. So as per documentation from data fetching make this function async
+
+  // 11.2 Note: Concept of caching.
+  // When we want to cache the blog data we will use following fetch method which will be saved but if any blog is updated it will not be updated. to check it run npm run build u will see two types of build  in terminal ○ (Static) pre-rendered as static content and ƒ  (Dynamic)  server-rendered on demand i.e static are saved and then npm run dev or start and go to home and then go to blog page. to check go to the network tab u will not see (blog?_rsc=bwuok) type of request because there is no data fetching in blogs as static that is blog is one time fetched (used inBlogs, posts, marketing pages). but if u want to get the updated blog u will add { cache: "no-store" } in fetch method. then go to the network tab u will see (blog?_rsc=bwuok) type of request again which is come from network when every time u go to the blog page (used in Admin pages, user dashboards, real-time stats). Best practice is make it static.
+  const response = await fetch(
+    "https://api.vercel.app/blog" /* {
+    cache: "no-store",
+  } */
+  );
+  const blogsCollection = await response.json();
   console.log(blogsCollection);
 
   return (
     <>
       <h3>Blog Page</h3>
-      {/* 10.2 map over the blogsCollection*/}
+      {/* 10.2 map over the blogsCollection using BlogCard component*/}
       <div>
         {blogsCollection.map((blog) => (
-          <div key={blog.id}>
-            <h3>{blog.title}</h3>
-            <button>
-              <Link href={`blog/${blog.id}`}>Blog Details</Link>
-            </button>
-          </div>
+          <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
     </>
---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+// 11.1 create a BlogCard component and pass blog as props
+
+export default function BlogCard({ blog }) {
+  return (
+    <div className="bg-white rounded-xl shadow-md p-5 border hover:shadow-lg transition">
+      <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
+
+      <p className="text-gray-600 text-sm mb-3">
+        {blog.content.slice(0, 90)}...
+      </p>
+
+      <div className="text-xs text-gray-500 mb-3">
+        <p>
+          Author: <span className="font-medium">{blog.author}</span>
+        </p>
+        <p>Date: {blog.date}</p>
+        <p className="mt-1 inline-block bg-blue-100 text-blue-600 px-2 py-1 rounded-md">
+          {blog.category}
+        </p>
+      </div>
+
+      <Link
+        href={`/blog/${blog.id}`}
+        className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md mt-3 hover:bg-blue-700 transition"
+      >
+        Read More
+      </Link>
+    </div>
+  );
+}
