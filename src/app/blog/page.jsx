5054051c5c89b4c24c05b78346afffb70794ae7f
ammarShahab <ashahab007@gmail.com>
2025-11-16 09:52:16 +0600

    Clarify comment on auto-fetching blogs in BlogPage component

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -30,7 +30,7 @@ export default async function BlogPage() {
   // 11.2 Note: Concept of caching.
   // When we want to cache the blog data we will use following fetch method which will be saved but if any blog is updated it will not be updated. to check it run npm run build u will see two types of build  in terminal ○ (Static) pre-rendered as static content and ƒ  (Dynamic)  server-rendered on demand i.e static are saved and then npm run dev or start and go to home and then go to blog page. to check go to the network tab u will not see (blog?_rsc=bwuok) type of request because there is no data fetching in blogs as static that is blog is one time fetched (used inBlogs, posts, marketing pages). but if u want to get the updated blog u will add { cache: "no-store" } in fetch method. then go to the network tab u will see (blog?_rsc=bwuok) type of request again which is come from network when every time u go to the blog page (used in Admin pages, user dashboards, real-time stats). Best practice is make it static.
 
-  // another thing u should know Here there is a something u see if u open ur network tab u will see the request is auto fetching the blogs upon scroll. which will like 24?_rsc=...., 25?_rsc=....
+  // another thing u should know Here there is a something u see if u open ur network tab u will see the request is auto fetching the blogs upon scroll. which will like 24?_rsc=...., 25?_rsc=.... Because next js load the data according to the viewport size and send the data in a chunk.
   const response = await fetch(
     "https://api.vercel.app/blog" /* {
     cache: "no-store",
